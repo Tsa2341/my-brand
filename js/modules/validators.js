b25ab93eb4985efc04d0df57d39648cb47ce6cef
Tsa2341 <alanshema2002@gmail.com>
2022-01-11 18:03:40 +0200

    chore(flight): added validation for signin page

---
+++ js/modules/validators.js
@@ -0,0 +1,26 @@
+
+export const emailValidator = (email) => {
+    const pattern = /^(.+)([a-z]+)([0-9]+)@(gmail)\.(com)\s*$/i;
+
+    return new Promise((resolve, reject) => {
+        if (email === '') {
+            return reject('Please, enter your email.');
+        } else {
+            return (pattern.test(email)) ? resolve('') : reject('Invalid email');
+        } 
+    })
+    
+}
+
+export const passwordValidator = (password) => {
+    const pattern = /^(\d|\w){7,}$/;
+
+    return new Promise((resolve, reject) => {
+        if (password === '') {
+            return reject('Please, enter your password.');
+        } else {
+            return (pattern.test(password)) ? resolve('') : reject('Invalid password\nPassword should be greater than 7');
+        } 
+    })
+    
+}
---
+++ js/signinScript.js
@@ -0,0 +1,59 @@
+import { emailValidator, passwordValidator } from "./modules/validators.js";
+
+
+const emailInput = document.getElementById('email')
+const passwordInput = document.getElementById('password')
+const emailValidatorEl = document.getElementById('email-validator')
+const passwordValidatorEl = document.getElementById('password-validator')
+const button = document.getElementById('button')
+
+const signinEmailValidator = async (func = ()=>{}) => {
+    await emailValidator(emailInput.value)
+        .then((msg) => {
+            emailValidatorEl.innerText = msg;
+            emailValidatorEl.classList.remove('email-validator');
+            button.disabled = false;
+            func();
+        }).catch((msg) => {
+            // console.log("email catch")
+            emailValidatorEl.innerText = msg;
+            emailValidatorEl.classList.add('email-validator');
+            button.disabled = true;
+        })
+    
+}
+
+const signinPasswordValidator = async (func = () => { }) => {
+    console.log(passwordInput.value)
+    await passwordValidator(passwordInput.value)
+        .then((msg) => {
+            // console.log("password then")
+            passwordValidatorEl.innerText = msg;
+            passwordValidatorEl.classList.remove('password-validator');
+            button.disabled = false;
+            func();
+        }).catch((msg) => {
+            // console.log("password catch")
+            passwordValidatorEl.innerText = msg;
+            passwordValidatorEl.classList.add('password-validator');
+            button.disabled = true;
+        })
+    
+}
+
+emailInput.addEventListener('keyup', (e) => {
+    signinEmailValidator();
+})
+
+
+passwordInput.addEventListener('keyup', (e) => {
+    signinPasswordValidator();
+})
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("email input value = ", emailInput, "password input value = ", passwordInput)
+
+    signinEmailValidator(() => signinPasswordValidator(() => window.location.assign("../dashboard.html")));
+
+})
