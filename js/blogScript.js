ec8d78b55eeef4d6b84aa9eccf98a9ca9d343048
Tsa2341 <alanshema2002@gmail.com>
2022-02-14 16:05:21 +0200

    chore: minor change in the design

--- js/blogScript.js
+++ js/blogScript.js
@@ -1,42 +1,41 @@
 
 
-const blogsEl1 = document.querySelector('.blogs1');
-const blogsEl2 = document.querySelector('.blogs2');
-const blogEls = document.getElementsByClassName('blog')
+const blogsEl = document.querySelector(".blogs-container");
+const blogEls = document.getElementsByClassName("blog");
 
+const blogs = localStorage.getItem("blogs")
+  ? Object.values(JSON.parse(localStorage.getItem("blogs")))
+  : [];
 
+blogs.map((blog, index) => {
+  const div1 = document.createElement("div");
+  div1.classList.add("blog");
+  let blogEl = "";
 
-const blogs = localStorage.getItem('blogs') ? Object.values(JSON.parse(localStorage.getItem('blogs'))) : [];
+  if (blogs === []) {
+    blogEl = `
+                <img id="image" src="../public/blog3.svg" alt="blog image">
+                <p class="blog-date">2022/09/10</p>
+                <h1 id="title" class="blog-title">no blog</h1>
+            `;
+  } else {
+    blogEl = `
+                    <img id="image" src="${blog.image}" alt="blog image">
+                    <p class="blog-date">2022/09/10</p>
+                    <h1 id="title" class="blog-title">${blog.title}</h1>
+                `;
+  }
 
-blogs.map((blog,index) => {
-
-    const div1 = document.createElement('div');
-    const div2 = document.createElement('div');
-
-
-    const blogEl = `
-                <a href="#" class="blog">
-                    <img id="image" src="${blog.image}" alt=" third blog">
-                    <h4 id="title">${blog.title}</h4>
-                </a>
-            `
-
-    div1.innerHTML = blogEl;
-    div2.innerHTML = blogEl;
-    if (index < 3) {
-        blogsEl1.appendChild(div1);
-    }
-    blogsEl2.appendChild(div2);
-})
-
+  div1.innerHTML = blogEl;
+  blogsEl.appendChild(div1);
+});
 
 //  add event listeners
 
 for (let i = 0; i < blogEls.length; i++) {
-    
-    blogEls[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        localStorage.setItem('current-blog', blogEls[i].children[1].textContent);
-        window.location.assign('../article.html');
-    })
+  blogEls[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    localStorage.setItem("current-blog", blogEls[i].children[2].textContent);
+    window.location.assign("../article.html");
+  });
 }
--- js/contactScript.js
+++ js/contactScript.js
@@ -5,129 +5,146 @@ const emailEl = document.getElementById('email')
 const descriptionEl = document.getElementById('description')
 const emailValidatorEl = document.getElementById('email-validator')
 const generalValidatorEl = document.getElementById('general-validator')
-const successEl = document.getElementById('success')
-const button = document.getElementById('button');
+const successEl = document.getElementById("button");
+const button = document.getElementById("button");
 
 //  create validating function and other functions
 
 const contactEmailValidator = async () => {
-    await emailValidator(emailEl.value)
-        .then((msg) => {
-            emailValidatorEl.innerText = msg;
-            emailValidatorEl.classList.remove('email-validator');
-            button.disabled = false;
-        }).catch((msg) => {
-            emailValidatorEl.innerText = msg;
-            emailValidatorEl.classList.add('email-validator');
-            button.disabled = true;
-        })
-    
-}
+  await emailValidator(emailEl.value)
+    .then((msg) => {
+      emailValidatorEl.innerText = msg;
+      emailValidatorEl.classList.remove("email-validator");
+      button.disabled = false;
+    })
+    .catch((msg) => {
+      emailValidatorEl.innerText = msg;
+      emailValidatorEl.classList.add("email-validator");
+      button.disabled = true;
+    });
+};
 
-const contactGeneralValidator = (func = ()=>{}) => {
-    spaceValidator(nameEl.value, emailEl.value, descriptionEl.value) ?
-        (
-            generalValidatorEl.innerText = "",
-            generalValidatorEl.classList.remove('general-validator'),
-            button.disabled = false,
-            func()
-        )
-        :
-        (
-            generalValidatorEl.innerText = " All fields must be field ",
-            generalValidatorEl.classList.add('general-validator'),
-            button.disabled = true
-        )
-        
-}
+const contactGeneralValidator = (func = () => {}) => {
+  spaceValidator(nameEl.value, emailEl.value, descriptionEl.value)
+    ? ((generalValidatorEl.innerText = ""),
+      generalValidatorEl.classList.remove("general-validator"),
+      (button.disabled = false),
+      func())
+    : ((generalValidatorEl.innerText = " All fields must be field "),
+      generalValidatorEl.classList.add("general-validator"),
+      (button.disabled = true));
+};
 
 const dateFormatter = () => {
-    return new Date().toLocaleString('en-GB', {
-        year: "numeric",
-        month: 'long',
-        day: '2-digit',
-        weekday: "long",
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    })
-}
+  return new Date().toLocaleString("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+};
 
 const addressFormatter = async () => {
-    const location = await locationGetter()
+  const location = await locationGetter();
 
-    if (!location) {
-        return "No location ( disabled by user)"
-    }
+  if (!location) {
+    return "No location ( disabled by user)";
+  }
 
-    const country = await location.results[0].components.country.trim()
-    const province = await location.results[0].components.state.split(' ')[0].trim()
-    const district = await location.results[0].components.county.split(' ')[0].trim()
-    const town = await location.results[0].components.town.split(' ').join('').trim()
+  console.log(location);
 
-    const address = [country, province, district, town].join('/ ');
-    return address;
-}
+  const country = await location.results[0].components.country.trim();
+  const province = await location.results[0].components.state
+    .split(" ")[0]
+    .trim();
+  const district = await location.results[0].components.county
+    .split(" ")[0]
+    .trim();
+  //   const town = await location.results[0].components.town
+  //     .split(" ")
+  //     .join("")
+  //     .trim();
+
+  const address = [country, province, district].join("/ ");
+  return address;
+};
 
 const locationGetter = () => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    const onSuccess = (res) => {
+      const { latitude, longitude } = res.coords;
 
-        const onSuccess = (res) => {
-            const { latitude, longitude } = res.coords;
+      fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=05ccf17adcef455c99dc2fd37a7648a4`
+      )
+        .then((data) => {
+          data.json().then(resolve);
+        })
+        .catch(reject);
+    };
 
-            fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=05ccf17adcef455c99dc2fd37a7648a4`)
-                .then(data => {
-                    data.json().then(resolve)
-                }).catch(reject)
-        }
+    const onError = (err) => {
+      // navigator.permissions.query({name:'geolocation'}).then(function(result) {
+      //     if (result.state == 'granted') {
+      //         report(result.state);
+      //     } else if (result.state == 'prompt') {
+      //         report(result.state);
+      //     } else if (result.state == 'denied') {
+      //         report(result.state);
+      //     }
+      //     result.onchange = function() {
+      //         report(result.state);
+      //     }
+      // });
+      resolve(false);
+    };
 
-        const onError = (err) => {
-            // navigator.permissions.query({name:'geolocation'}).then(function(result) {
-            //     if (result.state == 'granted') {
-            //         report(result.state);
-            //     } else if (result.state == 'prompt') {
-            //         report(result.state);
-            //     } else if (result.state == 'denied') {
-            //         report(result.state);
-            //     }
-            //     result.onchange = function() {
-            //         report(result.state);
-            //     }
-            // });
-            resolve(false)
-        }
-
-        window.navigator.geolocation.getCurrentPosition(onSuccess,onError)
-    })
-    
-}
+    window.navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  });
+};
 
 const saveMessage = async (name, email, description) => {
-    successEl.textContent = "Please wait , sending ..."
-    successEl.classList.add('success')
-    button.disabled = true;
-    
-    const prevQueries = localStorage.getItem("queries") ? Object.values(JSON.parse(localStorage.getItem("queries"))) : [];
+  successEl.textContent = "Please wait , sending ...";
+  successEl.classList.add("success");
+  //   button.disabled = true;
 
-    localStorage.setItem("queries", JSON.stringify({
-        ...[
-            ...prevQueries,
-            { name, email, description, date: dateFormatter(), location: await addressFormatter() }
-        ]
-    }));
+  const prevQueries = localStorage.getItem("queries")
+    ? Object.values(JSON.parse(localStorage.getItem("queries")))
+    : [];
 
-    successEl.textContent = "sent successfully"
-    setTimeout(() => {
-        successEl.textContent = ""
-        successEl.classList.remove('success')
-        emailEl.value = '';
-        nameEl.value = '';
-        descriptionEl.value = '';
-        button.disabled = false
-    }, 1500);
+  localStorage.setItem(
+    "queries",
+    JSON.stringify({
+      ...[
+        ...prevQueries,
+        {
+          name,
+          email,
+          description,
+          date: dateFormatter(),
+          location: await addressFormatter(),
+        },
+      ],
+    })
+  );
 
-    // console.log(JSON.parse(localStorage.getItem("queries")))
-}
+  console.log("success");
+
+  successEl.textContent = "sent successfully";
+  setTimeout(() => {
+    successEl.textContent = "send";
+    successEl.classList.remove("success");
+    emailEl.value = "";
+    nameEl.value = "";
+    descriptionEl.value = "";
+    button.disabled = false;
+  }, 1500);
+
+  // console.log(JSON.parse(localStorage.getItem("queries")))
+};
 
 // add eventlisteners on email and button
 
